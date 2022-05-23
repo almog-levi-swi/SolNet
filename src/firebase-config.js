80b992ce9489a6437aa1f1fa59518b9af1cca7d6
elad kaufman <elad.kaufman@solarwinds.com>
2022-05-23 14:15:14 +0300

    firebase-config

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCI5uyjXjjGnvMIMtpg3Mt3VnE39g8Ue5M",
+    authDomain: "solnat-698bf.firebaseapp.com",
+    databaseURL: "https://solnat-698bf-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "solnat-698bf",
+    storageBucket: "solnat-698bf.appspot.com",
+    messagingSenderId: "651684636578",
+    appId: "1:651684636578:web:3bc08c6439259cfb1af4fb",
+    measurementId: "G-XB2P8ZYPQ4"
+  };
+
+  const app = initializeApp(firebaseConfig)
