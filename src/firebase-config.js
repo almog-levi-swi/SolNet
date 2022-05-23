2f347b30fe490f704d479ef3864d08db4cdbe295
elad kaufman <elad.kaufman@solarwinds.com>
2022-05-23 14:33:02 +0300

    config without secret stuff

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCI5uyjXjjGnvMIMtpg3Mt3VnE39g8Ue5M",
-    authDomain: "solnat-698bf.firebaseapp.com",
-    databaseURL: "https://solnat-698bf-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "solnat-698bf",
-    storageBucket: "solnat-698bf.appspot.com",
-    messagingSenderId: "651684636578",
-    appId: "1:651684636578:web:3bc08c6439259cfb1af4fb",
-    measurementId: "G-XB2P8ZYPQ4"
-  };
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  databaseURL:
+    "https://solnat-698bf-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "solnat-698bf",
+  storageBucket: "solnat-698bf.appspot.com",
+  messagingSenderId: "651684636578",
+  appId: "1:651684636578:web:5a50c01478a12c361af4fb",
+  measurementId: "G-G0H5MP61J9",
+};
 
-  const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
