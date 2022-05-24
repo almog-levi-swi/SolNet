11a943a7ad0f59bccdaa56152a71bd2738bef642
neriya234 <neriya.fisch@solarwinds.com>
2022-05-24 17:49:47 +0300

    fix signup visabilty

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,7 +1,16 @@
 import DynamicKids from "./DynamicKids.js";
 import React, { useState } from "react";
 
-import { Button, Form, Input, Select, DatePicker, Checkbox } from "antd";
+import {
+  Button,
+  Form,
+  Input,
+  Select,
+  DatePicker,
+  Checkbox,
+  Row,
+  Col,
+} from "antd";
 
 const { Option } = Select;
 const birthDateConfig = {
@@ -70,6 +79,7 @@ const Signup = () => {
   const [kids, setKids] = useState(0);
   const [student, setStudent] = useState(false);
   const onFinish = (values) => {
+    console.log('Received values of form: ', values);
   };
   const prefixSelector = (
     <Form.Item name="prefix" noStyle>
@@ -97,66 +107,96 @@ const Signup = () => {
       }}
       scrollToFirstError
     >
-      <Form.Item
-        name="first name"
-        label="First Name"
-        rules={[
-          {
-            required: true,
-            message: "Please input your first name!",
-            whitespace: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+      <Row justify="center">
+        <Col span={5}>
+          <Form.Item
+            name="first name"
+            label="First Name"
+            rules={[
+              {
+                required: true,
+                message: "Please input your first name!",
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input placeholder="Your first name" />
+          </Form.Item>
+        </Col>
 
-      <Form.Item
-        name="last name"
-        label="Last Name"
-        rules={[
-          {
-            required: true,
-            message: "Please input your last name!",
-            whitespace: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+        <Col span={5}>
+          <Form.Item
+            name="last name"
+            label="Last Name"
+            rules={[
+              {
+                required: true,
+                message: "Please input your last name!",
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input placeholder="Your last name" />
+          </Form.Item>
+        </Col>
+      </Row>
 
-      <Form.Item
-        name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: "email",
-            message: "The input is not valid E-mail!",
-          },
-          {
-            required: true,
-            message: "Please input your E-mail!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+      <Row justify="center">
+        <Col span={5}>
+          <Form.Item
+            name="email"
+            label="E-mail"
+            rules={[
+              {
+                type: "email",
+                message: "The input is not valid E-mail!",
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!",
+              },
+            ]}
+          >
+            <Input placeholder="Your email" />
+          </Form.Item>
+        </Col>
 
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
+        <Col span={5}>
+          <Form.Item
+            name="address"
+            label="Address"
+            rules={[
+              {
+                required: true,
+                message: "Please input your address!",
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input placeholder="Your address" />
+          </Form.Item>
+        </Col>
+      </Row>
 
-      <Form.Item
+      <Row justify="center">
+        <Col span={5}>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password placeholder="Your password" />
+          </Form.Item>
+        </Col>
+
+        <Col span={5}>
+        <Form.Item
         name="confirm"
         label="Confirm Password"
         dependencies={["password"]}
@@ -181,198 +221,225 @@ const Signup = () => {
       >
         <Input.Password />
       </Form.Item>
+        </Col>
+      </Row>
 
-      <Form.Item name="birth date" label="Birth Date" {...birthDateConfig}>
-        <DatePicker />
-      </Form.Item>
+      <Row justify="center">
+        <Col span={5}>
+          <Form.Item name="birth date" label="Birth Date" {...birthDateConfig}>
+            <DatePicker />
+          </Form.Item>
+        </Col>
 
-      <Form.Item name="join date" label="Join Date" {...joinDateConfig}>
-        <DatePicker />
-      </Form.Item>
+        <Col span={5}>
+          <Form.Item name="join date" label="Join Date" {...joinDateConfig}>
+            <DatePicker />
+          </Form.Item>
+        </Col>
+      </Row>
 
-      <Form.Item
-        name="role"
-        label="Role"
-        rules={[
-          {
-            required: true,
-            message: "Please select role!",
-          },
-        ]}
-      >
-        <Select placeholder="select your role">
-          <Option value="regular">Regular</Option>
-          <Option value="team_leader">Team Leader</Option>
-          <Option value="group_leader">Group Leader</Option>
-          <Option value="ceo">CEO</Option>
-        </Select>
-      </Form.Item>
+      <Row justify="center">
+        <Col span={5}>
+          <Form.Item
+            name="department"
+            label="Department"
+            rules={[
+              {
+                required: true,
+                message: "Please select department!",
+              },
+            ]}
+          >
+            <Select placeholder="select your department">
+              <Option value="r&d">R&D</Option>
+              <Option value="automation">Automation</Option>
+              <Option value="devops">DevOps</Option>
+              <Option value="hr">HR</Option>
+            </Select>
+          </Form.Item>
+        </Col>
 
-      <Form.Item
-        name="department"
-        label="Department"
-        rules={[
-          {
-            required: true,
-            message: "Please select department!",
-          },
-        ]}
-      >
-        <Select placeholder="select your department">
-          <Option value="r&d">R&D</Option>
-          <Option value="automation">Automation</Option>
-          <Option value="devops">DevOps</Option>
-          <Option value="hr">HR</Option>
-        </Select>
-      </Form.Item>
+        <Col span={5}>
+          <Form.Item
+            name="role"
+            label="Role"
+            rules={[
+              {
+                required: true,
+                message: "Please select role!",
+              },
+            ]}
+          >
+            <Select placeholder="select your role">
+              <Option value="regular">Regular</Option>
+              <Option value="team_leader">Team Leader</Option>
+              <Option value="group_leader">Group Leader</Option>
+              <Option value="ceo">CEO</Option>
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
 
-      <Form.Item
-        name="shirt_size"
-        label="Shirt Size"
-        rules={[
-          {
-            required: true,
-            message: "Please select shirt size!",
-          },
-        ]}
-      >
-        <Select placeholder="select your shirt size">
-          <Option value="small">S</Option>
-          <Option value="medium">M</Option>
-          <Option value="large">L</Option>
-          <Option value="extra_large">XL</Option>
-          <Option value="extra_extra_large">XXL</Option>
-        </Select>
-      </Form.Item>
+      <Row justify="center">
+        <Col span={5}>
+          <Form.Item
+            name="shirt_size"
+            label="Shirt Size"
+            rules={[
+              {
+                required: true,
+                message: "Please select shirt size!",
+              },
+            ]}
+          >
+            <Select placeholder="select your shirt size">
+              <Option value="small">S</Option>
+              <Option value="medium">M</Option>
+              <Option value="large">L</Option>
+              <Option value="extra_large">XL</Option>
+              <Option value="extra_extra_large">XXL</Option>
+              <Option value="other">Other</Option>
+            </Select>
+          </Form.Item>
+        </Col>
 
-      <Form.Item
-        name="food_preferences"
-        label="Food Preferences"
-        rules={[
-          {
-            required: true,
-            message: "Please select food preferences!",
-          },
-        ]}
-      >
-        <Select placeholder="select your food preferences">
-          <Option value="none"> Eat everything</Option>
-          <Option value="kosher"> Kosher</Option>
-          <Option value="vegetarian">Vegetarian</Option>
-          <Option value="vegan">Vegan</Option>
-          <Option value="allergic">Allergic</Option>
-          <Option value="special">Special</Option>
-        </Select>
-      </Form.Item>
+        <Col span={5}>
+          <Form.Item
+            name="food_preferences"
+            label="Food Preferences"
+            rules={[
+              {
+                required: true,
+                message: "Please select food preferences!",
+              },
+            ]}
+          >
+            <Select placeholder="select your food preferences">
+              <Option value="none"> Eats everything</Option>
+              <Option value="kosher"> Kosher</Option>
+              <Option value="vegetarian">Vegetarian</Option>
+              <Option value="vegan">Vegan</Option>
+              <Option value="allergic">Allergic</Option>
+              <Option value="special">Special</Option>
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
 
-      <Form.Item
-        name="address"
-        label="Address"
-        rules={[
-          {
-            required: true,
-            message: "Please input your address!",
-            whitespace: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+      <Row justify="center">
+        <Col span={5}>
+          <Form.Item
+            name="phone"
+            label="Phone Number"
+            rules={[
+              {
+                required: true,
+                message: "Please input your phone number!",
+              },
+            ]}
+          >
+            <Input
+              addonBefore={prefixSelector}
+              style={{
+                width: "100%",
+              }}
+            />
+          </Form.Item>
+        </Col>
 
-      <Form.Item
-        name="phone"
-        label="Phone Number"
-        rules={[
-          {
-            required: true,
-            message: "Please input your phone number!",
-          },
-        ]}
-      >
-        <Input
-          addonBefore={prefixSelector}
-          style={{
-            width: "100%",
-          }}
-        />
-      </Form.Item>
+        <Col span={5}>
+          <Form.Item
+            name="gender"
+            label="Gender"
+            rules={[
+              {
+                required: true,
+                message: "Please select gender!",
+              },
+            ]}
+          >
+            <Select placeholder="select your gender">
+              <Option value="male">Male</Option>
+              <Option value="female">Female</Option>
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
 
-      <Form.Item
-        name="gender"
-        label="Gender"
-        rules={[
-          {
-            required: true,
-            message: "Please select gender!",
-          },
-        ]}
-      >
-        <Select placeholder="select your gender">
-          <Option value="male">Male</Option>
-          <Option value="female">Female</Option>
-        </Select>
-      </Form.Item>
+      <Row justify="center">
+        <Col span={5}>
+          <Form.Item
+            name="marrige"
+            label="Married?"
+            rules={[
+              {
+                required: true,
+                message: "Please select option!",
+              },
+            ]}
+          >
+            <Select placeholder="Are you married?">
+              <Option value="yes">Yes</Option>
+              <Option value="no">No</Option>
+            </Select>
+          </Form.Item>
+        </Col>
 
-      <Form.Item
-        name="marrige"
-        label="Married?"
-        rules={[
-          {
-            required: true,
-            message: "Please select option!",
-          },
-        ]}
-      >
-        <Select placeholder="Are you married?">
-          <Option value="yes">Yes</Option>
-          <Option value="no">No</Option>
-        </Select>
-      </Form.Item>
-
-      <Form.Item
-        name="num_of_kids"
-        label="Number of kids"
-        rules={[
-          {
-            required: true,
-            message: "Please select number of kids!",
-          },
-        ]}
-      >
-        <Select
-          placeholder="Number of kids?"
-          onSelect={(value) => {
-            setKids(value);
-          }}
-        >
-          <Option value="0">0</Option>
-          <Option value="1">1</Option>
-          <Option value="2">2</Option>
-          <Option value="3">3</Option>
-          <Option value="4">4</Option>
-          <Option value="5">5</Option>
-          <Option value="6">6</Option>
-          <Option value="7">7</Option>
-          <Option value="8">8</Option>
-        </Select>
-      </Form.Item>
+        <Col span={5}>
+          <Form.Item
+            name="num_of_kids"
+            label="Number of kids"
+            rules={[
+              {
+                required: true,
+                message: "Please select number of kids!",
+              },
+            ]}
+          >
+            <Select
+              placeholder="Number of kids?"
+              onSelect={(value) => {
+                setKids(value);
+              }}
+            >
+              <Option value="0">0</Option>
+              <Option value="1">1</Option>
+              <Option value="2">2</Option>
+              <Option value="3">3</Option>
+              <Option value="4">4</Option>
+              <Option value="5">5</Option>
+              <Option value="6">6</Option>
+              <Option value="7">7</Option>
+              <Option value="8">8</Option>
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
 
       <DynamicKids kids={kids} birthDateConfig={birthDateConfig} />
 
-      <Form.Item
-        name="student"
-        valuePropName="unchecked"
-        wrapperCol={{ offset: 6, span: 10 }}
-        onChange={(e) => setStudent(e.target.checked)}
-      >
-        <Checkbox>Student?</Checkbox>
-      </Form.Item>
-
-      {student ? (
-        <Form.Item name="end date" label="End Date" {...endDateConfig}>
-          <DatePicker />
-        </Form.Item>
-      ) : null}
+      <Row>
+        <Col span={3} offset={7}>
+          <Form.Item
+            name="student"
+            defaultChecked="unchecked"
+            wrapperCol={{ offset: 6, span: 10 }}
+            onChange={(e) => setStudent(e.target.checked)}
+          >
+            <label>
+              Student? 
+              <Checkbox/>
+            </label>
+          </Form.Item>
+        </Col>
+        {student ? (
+          <Col span={5} offset={2}>
+            <Form.Item name="end date" label="End Date" {...endDateConfig}>
+              <DatePicker />
+            </Form.Item>
+          </Col>
+        ) : null}
+      </Row>
 
       <Form.Item {...tailFormItemLayout}>
         <Button type="primary" htmlType="submit">
