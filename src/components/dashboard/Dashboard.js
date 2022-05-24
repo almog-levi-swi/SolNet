cccd395eed8695230d2ef118463b3881812e11eb
almog-levi-swi <levi.almog@solarwinds.com>
2022-05-24 15:26:32 +0300

    consts, dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Chart } from '../chart/Chart';
+import { GraphBy } from '../GraphBy/graphBy';
+import { DummyData } from '../../Consts/dummyData';
+import { groupByTypes } from '../../Consts/types';
+import { Divider, Col, Row, Card } from 'antd';
 
 export const Dashboard = () => {
+    const [graphBy, setGraphBy] = useState(groupByTypes.food);
+
     return (
-        <div>
-            Dashboard
-        </div>
+        <Card style={{ textAlign: 'center' }}
+            bordered={false}
+        >
+            <Divider orientation='center'><h1>Title</h1></Divider>
+            <Row justify='space-around' align='middle'>
+                <Col span={4}><h1>Events</h1></Col>
+                <Col span={8}>
+                    <GraphBy setGraphBy={setGraphBy} />
+                    <Chart data={DummyData} graphBy={graphBy} />
+                </Col>
+            </Row>
+            <Row justify='space-around' align='middle' style={{ textAlign: 'center' }}>
+                <Col span={12}><h1>Table</h1></Col>
+            </Row>
+
+        </Card>
     )
 }
 
