4ade1554d948e89848a2f3da02e5464cd4b82fbe
dor <dor.leventer@solarwinds.com>
2022-05-24 10:17:38 +0300

    change path and export to configuration file

--- src/components/dashboard/TableEmployees.js
+++ src/components/dashboard/TableEmployees.js
@@ -1,66 +1,8 @@
 import React, { useState } from "react";
 import { Table, Modal, Row, Col } from "antd";
+import { columns } from "./TableConfiguration";
 
 const TableEmployees = ({ employees }) => {
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      defaultSortOrder: "descend",
-      sorter: (a, b) => (a.name > b.name ? 1 : -1),
-      render: (text) => {
-        return <a>{text}</a>;
-      },
-    },
-    {
-      title: "Gender",
-      dataIndex: "gender",
-      defaultSortOrder: "descend",
-      sorter: (a, b) => (a.gender > b.gender ? 1 : -1),
-    },
-    {
-      title: "Birth Date",
-      dataIndex: "birthdate",
-      sorter: (a, b) => (a.birthdate > b.birthdate ? 1 : -1),
-    },
-    {
-      title: "Join Date",
-      dataIndex: "joindate",
-      sorter: (a, b) => (a.joindate > b.joindate ? 1 : -1),
-    },
-    {
-      title: "Role",
-      dataIndex: "role",
-      sorter: (a, b) => (a.role > b.role ? 1 : -1),
-    },
-    {
-      title: "Department",
-      dataIndex: "department",
-      sorter: (a, b) => (a.department > b.department ? 1 : -1),
-    },
-    {
-      title: "Phone",
-      dataIndex: "phone",
-      sorter: (a, b) => (a.phone > b.phone ? 1 : -1),
-    },
-    {
-      title: "Full Time",
-      dataIndex: "student",
-      sorter: (a, b) => (a.student > b.student ? 1 : -1),
-    },
-    {
-      title: "Graduation date",
-      dataIndex: "graduation",
-      sorter: (a, b) => (a.graduation > b.graduation ? 1 : -1),
-    },
-    {
-      title: "Address",
-      dataIndex: "address",
-      sorter: (a, b) => (a.address > b.address ? 1 : -1),
-    },
-  ];
-
-  //Modal should be deleted
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   const showModal = () => {
